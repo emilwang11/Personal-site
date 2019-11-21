f6205bd26b612ec8b820fcdd720b282addede92e
Emily Wang <emily@tdlab.io>
2019-11-21 14:38:03 -0500

    finished about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,14 @@
 <template>
   <b-container fluid class="px-0 mb-5  ">
-    <b-img
+    <b-col
+      col="auto"
+      sm="auto"
+      md="auto">
+      <b-img
        src="/img/Quote.png"
        class="quote m-4 p-4"
      ></b-img>
+    </b-col>
 
     <div>
       <h2 class="w-25"> About </h2>
@@ -11,46 +16,52 @@
     <b-container fluid class="home px-0">
         <b-row class="my-2">
           <b-col
-            sm=12
+            cols="8"
+            offset-cols="4"
+            sm="8"
+            offset-sm="2"
             md="3"
-            offset-md=1
+            offset-md="1"
           >
             <b-img
               fluid
               src="/img/about.png"
-              class="h-100 w-100 background-image"
+              class="background-image"
             ></b-img>
           </b-col>
           <b-col
             sm=12
             md=8
           >
-            <p class ="intro w-75">
-              My dream job as a child was to be an inventor. Not even knowing exactly what that meant,
+            <p class ="about-content w-75">
+              My dream job as a child was to be an inventor. Not even knowing exactly what that entailed,
               I just wanted to design and build things that solved people’s problem. I kept a sketchbook
               full of ideas for inventions/innovations from apps to newly designed backpacks. When I got older,
               I discovered that was essentially what an engineer did.
              </p>
-             <p class ="intro w-75 my-4">
-              Software has become one of the most powerful vehicles for change and I wanted to be able to learn to build something myself.
+             <p class ="about-content w-75 my-4">
+              Software has become one of the most powerful vehicles for change and I wanted to learn how to program to bring my ideas to life.
               </p>
-              <p class ="intro w-75 my-4">
+              <p class ="about-content w-75 my-4">
               Now with a technical foundation, I’m interested product management as it’s an intesection with my interests in business,
-              values empathy and the human aspect of engineering, and leverages my strong suit of critical thinking.
+              focuses on the human aspect of engineering, and leverages my strong suit of critical thinking.
             </p>
 
           </b-col>
         </b-row>
       </b-container>
-
-
   </b-container>
 </template>
 
 
 
 <style lang="scss">
-
+.about-content{
+  color: #3B3333;
+  font-size: 1.1em;
+  margin: auto;
+  text-align: left;
+}
 .quote{
   width: auto;
   height: 20%;
